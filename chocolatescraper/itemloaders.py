2c6a866724bdd6a545a62075f76fa99f42922071
wolathedataguy <adekogs85@gmail.com>
2024-12-11 05:04:57 +0100

    my nth try at using scrapy. PS: I learned scrapy before but never really did anything with it

---
+++ chocolatescraper/itemloaders.py
@@ -0,0 +1,7 @@
+from scrapy.loader import ItemLoader
+from itemloaders.processors import MapCompose, TakeFirst
+
+class ChocolateProductLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    price_in = MapCompose(lambda x: x.split("£")[-1])
+    url_in = MapCompose(lambda x: 'https://www.chocolate.co.uk' + x)
---
+++ chocolatescraper/spiders/chocolatespider.py
@@ -0,0 +1,61 @@
+import scrapy
+from chocolatescraper.items import ChocolateProduct
+from chocolatescraper.itemloaders import ChocolateProductLoader
+
+class ChocolatespiderSpider(scrapy.Spider):
+    name = "chocolatespider"
+    allowed_domains = ["chocolate.co.uk"]
+    start_urls = ["https://www.chocolate.co.uk/collections/all"]
+    
+    
+    
+    
+    
+    def parse(self, response):
+        products = response.css('product-item')
+        
+        for product in products:
+            
+                chocolate = ChocolateProductLoader(item=ChocolateProduct(), selector=product)
+                chocolate.add_css("name",'a.product-item-meta__title::text'),
+                chocolate.add_css("price", 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>'),
+                chocolate.add_css("url",'a.product-item-meta__title::attr(href)')
+                yield chocolate.load_item()
+            
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+        
+        if next_page is not None:
+            next_page_url = "https://www.chocolate.co.uk" + next_page
+            yield response.follow(next_page_url,callback = self.parse)
+
+    # def parse(self, response):
+    #     products = response.css('product-item')
+        
+    #     for product in products:
+    #         yield{
+    #             "name": product.css('a.product-item-meta__title::text').get(),
+    #             "price": product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
+    #             "url": product.css('a.product-item-meta__title').attrib['href']
+    #         }
+    #     next_page = response.css('[rel="next"] ::attr(href)').get()
+        
+    #     if next_page is not None:
+    #         next_page_url = "https://www.chocolate.co.uk" + next_page
+    #         yield response.follow(next_page_url,callback = self.parse)
+    
+    # def parse(self, response):
+    #     products = response.css('product-item')
+        
+    #     product_item = ChocolateProduct()
+    #     for product in products:
+            
+    #             product_item["name"]= product.css('a.product-item-meta__title::text').get(),
+    #             product_item["price"]= product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
+    #             product_item["url"]= product.css('a.product-item-meta__title').attrib['href']
+    #             yield product_item
+            
+    #     next_page = response.css('[rel="next"] ::attr(href)').get()
+        
+    #     if next_page is not None:
+    #         next_page_url = "https://www.chocolate.co.uk" + next_page
+    #         yield response.follow(next_page_url,callback = self.parse)
