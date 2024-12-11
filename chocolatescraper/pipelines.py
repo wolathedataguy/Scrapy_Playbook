81d253d05360ecaad4506253d0f3cd0d92f64dab
wolathedataguy <adekogs85@gmail.com>
2024-12-11 05:21:39 +0100

    Set up the duplicate items pipeline to remove duplicates

--- chocolatescraper/pipelines.py
+++ chocolatescraper/pipelines.py
@@ -26,3 +26,16 @@ class PriceToUSDPipeline:
         else:
             raise DropItem(f"Missing price in {item}")
 
+class DuplicatesPipeline:
+    
+    def __init__(self):
+        self.names_seen = set()
+    
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        
+        if adapter['name'] in self.names_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+            self.names_seen.add(adapter["name"])
+            return item
